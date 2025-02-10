e52196a1d245d84a4f3a44f71999b7de91820fa7
Jaganathan J <162098021+J-Jaganathan@users.noreply.github.com>
2025-02-10 20:19:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
-import pandas
+import pandas as pd
 st.title('This is a machine learning app for species prediction')
 
 st.info('This app holds and deploys machine learning model')
+with st.expander("Data"):
+  st.write("*Raw Data*");
+  df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv");
+  df
